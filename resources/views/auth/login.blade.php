2668a240f2d17a3547744f892a968f1bec1eb0c0
Roemer <bare0025@hz.nl>
2022-06-20 01:49:21 +0200

    breeze fix nr many

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="GET" action="{{ route('dashboard') }}">
             @csrf
 
             <!-- Email Address -->
