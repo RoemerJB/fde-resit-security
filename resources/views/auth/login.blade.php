df30d6254ec91301d7c50e7b920dd339f03233ea
Roemer <bare0025@hz.nl>
2022-06-20 02:07:53 +0200

    changed login form action to dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="HEAD" action="/">
+        <form method="POST" action="{{ route('dashboard') }}">
             @csrf
 
             <!-- Email Address -->
