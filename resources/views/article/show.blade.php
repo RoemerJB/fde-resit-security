b58bbbe9586501e8e1a5a6281c3f07b20e448e13
Roemer <bare0025@hz.nl>
2022-06-20 15:19:27 +0200

    added create and edit buttons to article

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,7 +1,10 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{$article->id}}</h1>
     <h2>{{$article->title}}</h2>
     <p>{{$article->body}}</p>
+
+    <form method="get" action="/article/edit">
+        <button type="submit">Edit</button>
+    </form>
 @endsection
