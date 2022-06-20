067f0ac2d52315f98cd0de39b5074b5c1f8d0a36
Roemer <bare0025@hz.nl>
2022-06-20 15:21:34 +0200

    fixed edit button to article

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,7 +4,7 @@
     <h2>{{$article->title}}</h2>
     <p>{{$article->body}}</p>
 
-    <form method="get" action="/article/edit">
+    <form method="get" action="/article/{{ $article->id }}/edit">
         <button type="submit">Edit</button>
     </form>
 @endsection
